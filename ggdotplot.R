f9b63f593d361240f6e6a466f9911542458b3727
Mike Taylor <mtaylor@semo.edu>
2017-12-10 18:55:24 -0600

    ggDot function works. Plots total or total and mean in order of total individuals. If plotting only mean, then not in proper order.

--- ggdotplot.R
+++ ggdotplot.R
@@ -1,18 +1,72 @@
 library(vegan)
 library(ggplot2)
+library(tidyverse)
 
 data("dune")
-tot_spp <- colSums(dune)
-spp_names <- names(tot_spp)
-tot_dot <- data.frame(spp_names, tot_spp)
+#tot_spp <- colSums(dune)
+#spp_names <- names(tot_spp)
+#tot_dot <- data.frame(spp_names, tot_spp)
 
+ggDot <- function(object,
+                  total = TRUE,
+                  mean = FALSE,
+                  xlabel = "Total Individuals",
+                  ...) {
+  if ((!isTRUE(total)) & (!isTRUE(mean)))
+    stop("Nothing to plot. Total or mean must be TRUE.")
+  
+  obj_names <- names(object)
+  obj_totals <- colSums(object)
+  obj_means <- colMeans(object)
+  obj_sds <- apply(object, 2, sd)
+  
+  #  obj_totals <- object %>% summarize_all(sum)
+  #  obj_means <- object %>% summarise_all(mean)
+  #  obj_sds <- object %>% summarise_all(sd)
+  
+  object <- tibble(obj_names, obj_totals, obj_means, obj_sds)
+  
+  p <- ggplot(object) +
+    theme_classic() +
+    theme(panel.grid.major.y = element_line(colour = "grey90",
+                                            linetype = "12")) +
+    ylab(NULL) +
+    xlab(xlabel)
+  
+  if (isTRUE(total)) {
+    p <-
+      p + geom_point(aes(x = obj_totals, y = reorder(obj_names, obj_totals)))
+  }
+  if (isTRUE(mean)) {
+    p <- p + geom_segment(
+      aes(
+        x = obj_means - obj_sds,
+        y = obj_names,
+        xend = obj_means + obj_sds,
+        yend = obj_names
+      ),
+      color = 'gray'
+    ) +
+      geom_point(
+        aes(x = obj_means,
+            y = reorder(obj_names, obj_totals)),
+        shape = 21,
+        color = 'gray',
+        fill = 'white'
+      )
+  }
+  return(p)
+}
+
+g <- ggDot(dune, total = TRUE, mean = FALSE, xlabel = "Number of Individuals")
+  
 ## Turn this into a function.
 ggplot(tot_dot,
        aes(x = tot_spp,
            y = reorder(spp_names, tot_spp))) +
   geom_point() +
   theme_classic() +
-  theme(panel.grid.major.y = element_line(colour = "grey80",
+  theme(panel.grid.major.y = element_line(colour = "grey90",
                                           linetype = "12")) +
   xlab("Total Individuals") +
   ylab(NULL)
